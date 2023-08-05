732a75ccdd2b44553237de262b8235ecce86a63f
Gabriel Augusto <gabrielaugustosergio@gmail.com>
2023-08-05 17:51:39 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  // no html, na lista de classe, contém 'light'? se sim remova
+  /*
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
+  */
+  // Mas há uma função do própria JS que faz a mesma coisa
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "assets/avatar-light.png")
+    img.setAttribute("alt", "Avatar light mode")
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "assets/avatar.png")
+    img.setAttribute("alt", "Avatar normal")
+  }
+}
